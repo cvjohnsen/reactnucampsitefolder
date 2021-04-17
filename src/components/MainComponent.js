f0e98a8eaa8a4ecdab5ea6b783ca5b1cc0439d0e
Your Name <cynthiajohnsen@Cynthias-MacBook-Pro-2.local>
2021-04-16 22:17:46 -0500

    Adding to the Single Page App

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,32 +4,46 @@ import CampsiteInfo from './CampsiteInfoComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
-import {Switch, Route, Redirect} from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Contact from './ContactComponent';
 import { CAMPSITES } from '../shared/campsites';
+import { COMMENTS } from '../shared/comments';
+import { PARTNERS } from '../shared/partners';
+import { PROMOTIONS } from '../shared/promotions';
 
 class Main extends Component {
+
     constructor(props) {
         super(props);
         this.state = {
             campsites: CAMPSITES,
+            comments: COMMENTS,
+            partners: PARTNERS,
+            promotions: PROMOTIONS
         };
     }
 
     render() {
         // Create locally scope component w/ arrow function, means only accessible in MainComponent
+        // For below reason, Arrow functions inherit the this of their parent scope
         const HomePage = () => {
             return (
-                <Home />
+                <Home
+                    campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
+                    promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
+                    partner={this.state.partners.filter(partner => partner.featured)[0]}
+                />
             );
-        }
+        };
 
         return (
             <div>
                 <Header />
                 <Switch>
-                    <Route path= '/home' component={HomePage}/>
-                    <Route exact path= '/directory' render={() => <Directory campsites={this.state.campsites} />} />
-                    <Redirect to ='/home'/>
+                    <Route path='/home' component={HomePage} />
+                    <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                    <Route exact path='/contactus' component={Contact} />
+                    <Redirect to='/home' />
                 </Switch>
                 <Footer />
             </div>
