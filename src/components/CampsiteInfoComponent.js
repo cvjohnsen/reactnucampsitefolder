9ba1ce6851578cb75c973a91ea9a3fc1188c38d3
Your Name <cynthiajohnsen@Cynthias-MacBook-Pro-2.local>
2021-04-29 23:54:46 -0500

    Redux Thunk

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -7,6 +7,8 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
 import ModalBody from 'reactstrap/lib/ModalBody';
 import ModalHeader from 'reactstrap/lib/ModalHeader';
 import Label from 'reactstrap/lib/Label';
+import { Loading } from './LoadingComponent';
+
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || (val.length <= len);
@@ -126,6 +128,26 @@ function RenderComments({ comments, addComment, campsiteId }) {
 }
 
 function CampsiteInfo(props) {
+    if (props.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    if (props.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            </div>
+        );
+    }
     if (props.campsite) {
              return (
             <div className="container">
