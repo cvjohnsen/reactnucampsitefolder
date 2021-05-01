9217845d1e3b491f85ca735939770fbb1260d9c2
Your Name <cynthiajohnsen@Cynthias-MacBook-Pro-2.local>
2021-05-01 11:20:02 -0500

    For Daniel

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,32 +1,59 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
+import { Fade, Stagger } from 'react-animation-components';
 
 
 function About(props) {
-    const partners = props.partners.map(partner => {
+
+function RenderPartner({ partner }) {
+    if (partner) {
         return (
-            <Media tag="li" key={partner.id}>
-                <RenderPartner partner= {partner} />
-            </Media>
+            <React.Fragment>
+                <Media object src={baseUrl + partner.image} alt={partner.name} width="150" />
+                <Media body className="ml-5 mb-4">
+                    <Media heading>{partner.name}</Media>
+                    {partner.description}
+                </Media>
+            </React.Fragment>
+        );
+    }
+    return (
+        <div></div>
+    );
+    }
+
+function PartnerList(props) {
+    const partners = props.partners.partners.map(partner => {
+        return (
+                <Media tag="li" >
+                    <RenderPartner partner={partner} />
+                </Media> 
         );
     });
 
-function RenderPartner ({partner}) {
-if (partner) {
+    if (props.partners.isLoading) {
+        return (
+            <Loading />
+        );
+    }
+    if (props.partners.errMess) {
+        return (
+            <div className="col">
+            <h4>{props.partners.errMess}</h4>
+            </div>
+        );
+    }
     return (
-        <React.Fragment>
-            <Media object src={partner.image} alt={partner.name} width="150"/>
-            <Media body className="ml-5 mb-4"> 
-            <Media heading > {partner.name}</Media>
-            {partner.description} 
+        <div className="col mt-4">
+            <Media list>
+                {partners}
             </Media>
-            <div/>
-        </React.Fragment>
-    );
+        </div>
+    )
 }
-}
-
     return (
         <div className="container">
             <div className="row">
@@ -79,14 +106,22 @@ if (partner) {
                 <div className="col-12">
                     <h3>Community Partners</h3>
                 </div>
-                <div className="col mt-4">
+                {/* <div className="col mt-4">
                     <Media list>
                         {partners}
+                    </Media>
+                </div> */}
+                <div className="col mt-4">
+                    <Media list>
+                        <PartnerList
+                            partners={props.partners}
+                        />
                     </Media>
                 </div>
             </div>
         </div>
     );
 }
+
 
 export default About;
